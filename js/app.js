009e55d6386acb3f6e2ef20e7e9c7a8ab6884bc9
Kyle Kellogg <kyle@kylekellogg.com>
2015-02-18 12:01:04 -0500

    Cleaned up app.js

--- js/app.js
+++ js/app.js
@@ -1,63 +1,51 @@
+$(document).ready(function () {
+  /*=========================*/
+  /*====main navigation hover dropdown====*/
+  /*==========================*/
+  $('.js-activated').dropdownHover({
+    instantlyCloseOthers: false,
+    delay: 0
+  }).dropdown();
 
+  /*=========================*/
+  /*====flex main slider====*/
+  /*==========================*/
+  $('.slider-main,.testimonials').flexslider({
+    slideshowSpeed: 3000,
+    directionNav: false,
+    animation: "fade"
+  });
 
+  /*=========================*/
+  /*========portfolio mix====*/
+  /*==========================*/
+  $('#grid').mixitup();
 
- /*=========================*/
-     /*====main navigation hover dropdown====*/
-     /*==========================*/
-    $(document).ready(function () {
-	
-	$('.js-activated').dropdownHover({
-		instantlyCloseOthers: false,
-		delay: 0
-	}).dropdown();
-	
-});
+  /*=========================*/
+  /*========tooltip and popovers====*/
+  /*==========================*/
+  $("[data-toggle=popover]").popover();
 
- /*=========================*/
-     /*====flex main slider====*/
-     /*==========================*/
-    $('.slider-main,.testimonials').flexslider({
-      slideshowSpeed: 3000,
-      directionNav: false,
-      animation: "fade"
-    });
-    
-     /*=========================*/
-     /*========portfolio mix====*/
-     /*==========================*/
-    $('#grid').mixitup();
-    
-       /*=========================*/
-     /*========tooltip and popovers====*/
-     /*==========================*/
-    $("[data-toggle=popover]").popover();
-    
-    $("[data-toggle=tooltip]").tooltip();
+  $("[data-toggle=tooltip]").tooltip();
 
-   /*=========================*/
-     /*========flex-gallery slide====*/
-     /*==========================*/
- $(window).load(function() {
-$('.flexslider').flexslider({
+  /*=========================*/
+  /*========flex-gallery slide====*/
+  /*==========================*/
+  $('.flexslider').flexslider({
     directionNav: false,
     slideshowSpeed: 3000,
     animation: "fade"
+  });
+
+  /* ==============================================
+  WOW plugin triggers animation.css on scroll
+  =============================================== */
+
+  var wow = new WOW({
+      boxClass:     'wow',      // animated element css class (default is wow)
+      animateClass: 'animated', // animation css class (default is animated)
+      offset:       150,        // distance to the element when triggering the animation (default is 0)
+      mobile:       false       // trigger animations on mobile devices (true is default)
+    });
+  wow.init();
 });
-});
-
-/* ==============================================
-WOW plugin triggers animation.css on scroll
-=============================================== */
-
-var wow = new WOW(
-  {
-    boxClass:     'wow',      // animated element css class (default is wow)
-    animateClass: 'animated', // animation css class (default is animated)
-    offset:       150,          // distance to the element when triggering the animation (default is 0)
-    mobile:       false        // trigger animations on mobile devices (true is default)
-  }
-);
-wow.init();
-
-
-
